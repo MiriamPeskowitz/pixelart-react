00c48fe1a3fb570805cbc3bfcd89d24a1dcc92c9
MiriamPeskowitz <miriam5000@gmail.com>
2020-10-27 22:25:47 -0400

    Install bootstrap

---
+++ react-redux-jwt-auth/src/actions/message.js
@@ -0,0 +1,13 @@
+//actions  related to messages from APIs'
+
+import { SET_MESSAGE, CLEAR_MESSAGE } from './types'
+
+export const setMessage = (message) => ({
+	type: SET_MESSAGE, 
+	payload: message,
+})
+
+export const clearMessage = () => ({
+	type: CLEAR_MESSAGE
+})
+
